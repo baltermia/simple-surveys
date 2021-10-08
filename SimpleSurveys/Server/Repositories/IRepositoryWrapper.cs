bc0af847556ca0ed15f47cc4dbf4ad71280644c3
speyck <54800864+speyck@users.noreply.github.com>
2021-10-08 10:48:30 +0200

    Add RepositoryWrapper class & interface

---
+++ SimpleSurveys/Server/Repositories/IRepositoryWrapper.cs
@@ -0,0 +1,13 @@
+﻿using SimpleSurveys.Shared.Models;
+
+namespace SimpleSurveys.Server.Repositories
+{
+    public interface IRepositoryWrapper
+    {
+        IRepository<Survey> Survey { get; }
+        IRepository<Step> Step { get; }
+        IRepository<SurveyType> StepType{ get; }
+        IRepository<SurveyType> SurveyType { get; }
+        void Save();
+    }
+}
---
+++ SimpleSurveys/Server/Repositories/RepositoryWrapper.cs
@@ -0,0 +1,28 @@
+﻿using SimpleSurveys.Shared.Models;
+
+namespace SimpleSurveys.Server.Repositories
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private readonly SimpleSurveysContext context;
+
+        private IRepository<Survey>     _survey;
+        private IRepository<Step>       _step;
+        private IRepository<SurveyType> _surveyType;
+        private IRepository<SurveyType> _stepType;
+
+        public IRepository<Survey>      Survey      =>  CreateIfNull(ref _survey);
+        public IRepository<Step>        Step        =>  CreateIfNull(ref _step);
+        public IRepository<SurveyType>  SurveyType  =>  CreateIfNull(ref _surveyType);
+        public IRepository<SurveyType>  StepType    =>  CreateIfNull(ref _stepType);
+
+        public RepositoryWrapper(SimpleSurveysContext context)
+        {
+            this.context = context;
+        }
+
+        public void Save() => context.SaveChanges();
+
+        private IRepository<T> CreateIfNull<T>(ref IRepository<T> repository) where T : class => repository ??= new Repository<T>(context);
+    }
+}
