b58878febaf5741c8e4b9698c8fca9e83f7d92f1
speyck <54800864+speyck@users.noreply.github.com>
2021-10-12 10:25:42 +0200

    Create Value.cs

---
+++ SimpleSurveys/Shared/Models/Value.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSurveys.Shared.Models
+{
+    public class Value : EntityID
+    {
+        [Required]
+        public string Text { get; set; }
+
+        public ICollection<StepResult_Value> StepResult_Values { get; set; }
+        public ICollection<Step_Value> Step_Values { get; set; }
+    }
+}
