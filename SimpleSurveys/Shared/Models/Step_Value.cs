03caf739b344cc6d50d0a4e109958a25409ed29f
speyck <54800864+speyck@users.noreply.github.com>
2021-10-12 10:25:09 +0200

    Create Step_Value.cs

---
+++ SimpleSurveys/Shared/Models/Step_Value.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleSurveys.Shared.Models
+{
+    public class Step_Value : EntityID
+    {
+        public int StepID { get; set; }
+        public int ValueID { get; set; }
+
+        public Step Step { get; set; }
+        public Value Value { get; set; }
+    }
+}
