ef1b095f5870f9a982c16652a75e65614234270a
speyck <54800864+speyck@users.noreply.github.com>
2021-10-13 10:53:28 +0200

    Move csharp into .cs file

---
+++ SimpleSurveys/Client/Pages/Index.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleSurveys.Shared.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace SimpleSurveys.Client.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private async void Click()
+        {
+            Survey survey = new()
+            {
+                Description = "The first survey",
+                Public = true,
+                Title = "The very first Survey!"
+            };
+
+            Step step = new Text()
+            {
+                Title = "This is the first step! Do you like it?",
+                Placeholder = "Put your answer here :)",
+                Required = true
+            };
+
+            survey.Steps.Add(step);
+
+            HttpResponseMessage response = await Http.PostAsJsonAsync("api", survey);
+        }
+    }
+}
