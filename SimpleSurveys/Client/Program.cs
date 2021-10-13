59220d7f07ef187e6e214e37d40dfa5a058329e3
speyck <54800864+speyck@users.noreply.github.com>
2021-10-13 10:54:42 +0200

    Add AntDesign

--- SimpleSurveys/Client/Program.cs
+++ SimpleSurveys/Client/Program.cs
@@ -19,6 +19,8 @@ namespace SimpleSurveys.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddAntDesign();
+
             await builder.Build().RunAsync();
         }
     }
