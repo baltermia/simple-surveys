7188fc57babc0eaaa729c90ff243928d9c5ab3de
speyck <54800864+speyck@users.noreply.github.com>
2021-10-26 07:57:18 +0200

    Add Client.Utils.Enums class

---
+++ SimpleSurveys/Client/Utils/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleSurveys.Client.Utils
+{
+    public class Enums
+    {
+        public enum Mode
+        {
+            View = 0,
+            Create = 1,
+            Edit = 2,
+            Fill = 3
+        }
+    }
+}
