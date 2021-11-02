0bfc98b04dc9ef4abff4ee07032678124ece9e35
speyck <54800864+speyck@users.noreply.github.com>
2021-11-02 14:04:53 +0100

    Create SurveyEdit

---
+++ SimpleSurveys/Client/Pages/SurveyEdit.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleSurveys.Shared.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SimpleSurveys.Client.Pages
+{
+    public partial class SurveyEdit
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        private Survey SurveyItem { get; set; }
+
+        protected async override Task OnParametersSetAsync()
+        {
+            SurveyItem = await Http.GetFromJsonAsync<Survey>("api/" + Id);
+
+            await base.OnParametersSetAsync();
+        }
+    }
+}
