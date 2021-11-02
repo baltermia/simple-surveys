ec80aa1d7fe7f81460115d5174ff16d94c52183c
speyck <54800864+speyck@users.noreply.github.com>
2021-11-02 14:05:21 +0100

    Create SurveyPreview

---
+++ SimpleSurveys/Client/Components/SurveyPreview.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SimpleSurveys.Client.Components
+{
+    public partial class SurveyPreview
+    {
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+
+        [Parameter]
+        public SimpleSurveys.Shared.Models.Survey SurveyItem { get; set; }
+    }
+}
