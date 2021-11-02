3272df44b4bb1f247a46964d5bf3425a0ebe7156
speyck <54800864+speyck@users.noreply.github.com>
2021-11-02 14:10:06 +0100

    Update Index cs

--- SimpleSurveys/Client/Pages/Index.razor.cs
+++ SimpleSurveys/Client/Pages/Index.razor.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using SimpleSurveys.Shared.Models;
-using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -13,37 +12,11 @@ namespace SimpleSurveys.Client.Pages
         [Inject]
         public HttpClient Http { get; set; }
 
-        private Survey surveyObj;
+        private List<Survey> Surveys;
 
-        private bool show = false;
-
-        protected override void OnInitialized()
+        protected async override Task OnParametersSetAsync()
         {
-            surveyObj = new()
-            {
-                Title = "How do you like the website?",
-                Description = "The very first survey, in which you can let us know how you like the site.",
-                Password = "survey1",
-                Updated = DateTime.Now.AddDays(4),
-                MaxSubmissions = 10,
-                Steps = new HashSet<Step>()
-                {
-                    new Text()
-                    {
-                        Title = "What do you like about the site?",
-                        Required = true,
-                        Placeholder = "Be honest :)"
-                    },
-                    new Text()
-                    {
-                        Title = "What do you NOT like about the site?",
-                        Required = true,
-                        Placeholder = "Now you can be mean ;)"
-                    }
-                }
-            };
-
-            show = true;
+            Surveys = await Http.GetFromJsonAsync<List<Survey>>("api");
         }
     }
 }
