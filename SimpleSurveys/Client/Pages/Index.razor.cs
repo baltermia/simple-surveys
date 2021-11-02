c0b5dddc15406d48ad2b0c400b10ed86f5e4debe
speyck <54800864+speyck@users.noreply.github.com>
2021-11-02 15:58:11 +0100

    Add grid

--- SimpleSurveys/Client/Pages/Index.razor.cs
+++ SimpleSurveys/Client/Pages/Index.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using SimpleSurveys.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -16,7 +17,46 @@ namespace SimpleSurveys.Client.Pages
 
         protected async override Task OnParametersSetAsync()
         {
-            Surveys = await Http.GetFromJsonAsync<List<Survey>>("api");
+            //Surveys = await Http.GetFromJsonAsync<List<Survey>>("api");
+            await Task.CompletedTask;
+
+            Surveys = new();
+
+            Survey survey = new Survey()
+            {
+                Title = "How do you like the website?",
+                Description = "The very first survey, in which you can let us know how you like the site.",
+                Password = "survey1",
+                Updated = DateTime.Now.AddDays(4),
+                MaxSubmissions = 10,
+                Steps = new HashSet<Step>()
+                {
+                    new Text()
+                    {
+                        Title = "What do you like about the site?",
+                        Required = true,
+                        Placeholder = "Be honest :)"
+                    },
+                    new Text()
+                    {
+                        Title = "What do you NOT like about the site?",
+                        Required = true,
+                        Placeholder = "Now you can be mean ;)"
+                    }
+                }
+            };
+
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
+            Surveys.Add(survey);
         }
     }
 }
