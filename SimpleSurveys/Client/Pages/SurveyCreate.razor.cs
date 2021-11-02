7e93e03cb015e6f7a44d706e3484b51cfc56889f
speyck <54800864+speyck@users.noreply.github.com>
2021-11-02 14:03:47 +0100

    Create SurveyCreate

---
+++ SimpleSurveys/Client/Pages/SurveyCreate.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleSurveys.Shared.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleSurveys.Client.Pages
+{
+    public partial class SurveyCreate
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private Survey SurveyItem { get; set; }
+
+        protected async override Task OnParametersSetAsync()
+        {
+            SurveyItem = new();
+
+            await base.OnParametersSetAsync();
+        }
+    }
+}
