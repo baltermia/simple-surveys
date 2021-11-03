0d0c627b11c5f09a397c5fa66c4ebde6f9a8035c
speyck <54800864+speyck@users.noreply.github.com>
2021-11-03 09:57:45 +0100

    Create HttpClientExtensions

---
+++ SimpleSurveys/Client/Utils/HttpClientExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSurveys.Client.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static JsonSerializerSettings settings = new()
+        {
+            TypeNameHandling = TypeNameHandling.Auto
+        };
+
+        public static async Task<T> GetBasicAsync<T>(this HttpClient client, string url)
+        {
+            HttpResponseMessage result = await client.GetAsync(url);
+
+            string resultJson = await result.Content.ReadAsStringAsync();
+
+            T value = JsonConvert.DeserializeObject<T>(resultJson, settings);
+
+            return value;
+        }
+
+        public static async Task<T> PostBasicAsync<T>(this HttpClient client, string url, T value)
+        {
+            string json = JsonConvert.SerializeObject(value, settings);
+
+            StringContent content = GetContent(json);
+
+            HttpResponseMessage result = await client.PostAsync(url, content);
+
+            string resultJson = await result.Content.ReadAsStringAsync();
+
+            T created = JsonConvert.DeserializeObject<T>(resultJson, settings);
+
+            return created;
+        }
+
+        public static async Task<T> PutBasicAsync<T>(this HttpClient client, string url, T value)
+        {
+            string json = JsonConvert.SerializeObject(value, settings);
+
+            StringContent content = GetContent(json);
+
+            HttpResponseMessage result = await client.PutAsync(url, content);
+
+            string resultJson = await result.Content.ReadAsStringAsync();
+
+            T updated = JsonConvert.DeserializeObject<T>(resultJson, settings);
+
+            return updated;
+        }
+
+        private static StringContent GetContent(string json) => new StringContent(json, Encoding.UTF8, "application/json");
+    }
+}
