af27664f03dbc0822f4c97711c720caf3ff5ed1a
speyck <54800864+speyck@users.noreply.github.com>
2021-11-04 09:22:14 +0100

    Remove NavigationManager

--- SimpleSurveys/Client/Components/SurveyPreview.razor.cs
+++ SimpleSurveys/Client/Components/SurveyPreview.razor.cs
@@ -4,9 +4,6 @@ namespace SimpleSurveys.Client.Components
 {
     public partial class SurveyPreview
     {
-        [Inject]
-        public NavigationManager NavManager { get; set; }
-
         [Parameter]
         public SimpleSurveys.Shared.Models.Survey SurveyItem { get; set; }
     }
